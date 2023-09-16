2425938ec07754495058147fffd69d2d495c5d4f
Dawit-Zeyede <devazeyede10@gmail.com>
2023-09-16 04:50:39 -0700

    formatted printer

--- main.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-int main(void)
-{
-	printf("\n%d\n", printf("Hello my name is %s, I am an %c student", "Haftom", 'A'));
-	printf("\n%d\n", _printf("Hello my name is %s, I am an %c student", "Haftom", 'A'));
-	return (0);
-}
