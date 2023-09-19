f9880601345274a9b797638d9830766bb522a866
Dawit-Zeyede <devazeyede10@gmail.com>
2023-09-18 22:06:04 -0700

    Formatted printer

--- _printf.c
+++ _printf.c
@@ -23,11 +23,11 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind] = format[i];
+			buff_ind += 1;
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			printed_chars += 1;
 		}
 		else
 		{
@@ -59,8 +59,9 @@ int _printf(const char *format, ...)
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+	int i;
 
+	for(i = 0; i <= *buff_ind; i++)
+		write(1, &buffer[i], 1);
 	*buff_ind = 0;
 }
